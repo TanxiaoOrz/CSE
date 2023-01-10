0e58b161ad66d5cd6b80c2a0343e08f1f6d56fee
Tanxiao <13671985248@163.com>
2023-01-10 19:48:53 +0800

    Initial commit

---
+++ src/test/java/com/example/cse/CseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
