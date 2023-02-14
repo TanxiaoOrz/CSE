e3c3549d05f50bb30d1ce7940112d64e2468910d
Tanxiao <13671985248@163.com>
2023-02-14 23:43:35 +0800

    新增对于User的新增url-post的拦截排除

--- src/main/java/com/example/cse/Config/WebMvcConfig.java
+++ src/main/java/com/example/cse/Config/WebMvcConfig.java
@@ -17,6 +17,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport{
@@ -24,37 +25,62 @@ public class WebMvcConfig extends WebMvcConfigurationSupport{
 
     TokenServiceImpl tokenService;
 
+    protected boolean checkToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String token = request.getHeader("token");
+        String description;
+        try {
+            DecodedJWT decodedJWT = tokenService.verifyToken(token);
+            if (tokenService.checkTokenType(decodedJWT, TokenServiceImpl.UserType)){
+                UserDto userDto= tokenService.getUserByToken(decodedJWT);
+                request.setAttribute("UserDto",userDto);
+                return true;
+            }else{
+                description = "错误的权限请求!!!";
+            }
+        }catch (TokenExpiredException e) {
+            description = "Token已经过期!!!";
+        } catch (SignatureVerificationException e) {
+            description = "签名错误!!!";
+        } catch (AlgorithmMismatchException e){
+            description = "加密算法不匹配!!!";
+        } catch (Exception e) {
+            e.printStackTrace();
+            description = "无效token~~";
+        }
+        Vo<String> vo = new Vo<>(Vo.NoAuthority,null,description);
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().println(new Gson().toJson(vo));
+
+        return false;
+    }
+
+    class UserCheck implements HandlerInterceptor {
+        @Override
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+            if (request.getMethod().equals("post")){
+                return true;
+            }else {
+                return checkToken(request,response);
+            }
+        }
+
+        @Override
+        public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+            HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+        }
+
+        @Override
+        public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+            HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+        }
+    }
+
     class TokenCheck implements HandlerInterceptor {
 
         @Override
         public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-            String token = request.getHeader("token");
-            String description;
-            try {
-                DecodedJWT decodedJWT = tokenService.verifyToken(token);
-                if (tokenService.checkTokenType(decodedJWT, TokenServiceImpl.UserType)){
-                    UserDto userDto= tokenService.getUserByToken(decodedJWT);
-                    request.setAttribute("UserDto",userDto);
-                    return true;
-                }else{
-                    description = "错误的权限请求!!!";
-                }
-            }catch (TokenExpiredException e) {
-                description = "Token已经过期!!!";
-            } catch (SignatureVerificationException e) {
-                description = "签名错误!!!";
-            } catch (AlgorithmMismatchException e){
-                description = "加密算法不匹配!!!";
-            } catch (Exception e) {
-                e.printStackTrace();
-                description = "无效token~~";
-            }
-            Vo<String> vo = new Vo<>(Vo.NoAuthority,null,description);
-
-            response.setContentType("application/json;charset=UTF-8");
-            response.getWriter().println(new Gson().toJson(vo));
-
-            return false;
+            return checkToken(request,response);
         }
 
         @Override
@@ -71,8 +97,20 @@ public class WebMvcConfig extends WebMvcConfigurationSupport{
     @Override
     protected void addInterceptors(InterceptorRegistry registry){
        registry.addInterceptor(new TokenCheck())
-               .addPathPatterns("/cse/**")
+               .addPathPatterns("/cse/User/**")
+               .excludePathPatterns("/cse/User")
                .excludePathPatterns("/cse/Token/**")//不需要拦截的地
+               .excludePathPatterns("/favicon.ico")
+               .excludePathPatterns("/swagger-ui.html/**",
+                       "/swagger-ui/**",
+                       "/swagger-resources/**",
+                       "/v2/api-docs",
+                       "/v3/api-docs",
+                       "/v3/api-docs/swagger-config",
+                       "/webjars/**",
+                       "/doc.html");
+       registry.addInterceptor(new UserCheck())
+               .addPathPatterns("/cse/User")
                .excludePathPatterns("/favicon.ico")
                .excludePathPatterns("/swagger-ui.html/**",
                        "/swagger-ui/**",
