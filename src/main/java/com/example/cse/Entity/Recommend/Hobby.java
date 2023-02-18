f45858427c65338c9a7f419b40a2508b90e8b51e
Tanxiao <13671985248@163.com>
2023-02-19 00:48:27 +0800

    Hobby.java 完成实体方法

--- src/main/java/com/example/cse/Entity/Recommend/Hobby.java
+++ src/main/java/com/example/cse/Entity/Recommend/Hobby.java
@@ -5,8 +5,46 @@ public class Hobby {
     private Integer Hid;//
     private String Name;//
     private String Description;//
-    private Integer Type;//
+    private String Type;//
     private String Model;//
 
-    private com.example.cse.Utils.Model model;
+    public Integer getHid() {
+        return Hid;
+    }
+
+    public void setHid(Integer hid) {
+        Hid = hid;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public String getModel() {
+        return Model;
+    }
+
+    public void setModel(String model) {
+        Model = model;
+    }
 }
