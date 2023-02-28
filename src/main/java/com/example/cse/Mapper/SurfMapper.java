0a2aa8240a77b1cdd2fe349324e10f8adc696c6f
Tanxiao <13671985248@163.com>
2023-02-28 20:55:50 +0800

    SurfMapper 新增获取浏览的统计

--- src/main/java/com/example/cse/Mapper/SurfMapper.java
+++ src/main/java/com/example/cse/Mapper/SurfMapper.java
@@ -3,6 +3,7 @@ package com.example.cse.Mapper;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface SurfMapper {
@@ -14,4 +15,22 @@ public interface SurfMapper {
 
     @Insert("insert into surf_message (Uid, Surf) VALUES (#{Uid},#{Lid})")
     Integer newLocationSurf(@Param("Uid") Integer uid,@Param("Lid") Integer lid);
+
+    @Select("select avg(counts) from surf_count_message")
+    Integer getAverageSurfCountMessage();
+
+    @Select("select avg(counts) from surf_count_location")
+    Integer getAverageSurfCountLocation();
+
+    @Select("select avg(counts) from surf_count_information_class")
+    Integer getAverageSurfCountInformationClass();
+
+    @Select("select counts from surf_count_message where surf = #{surf}")
+    Integer getSurfCountMessage(@Param("surf") Integer surf);
+
+    @Select("select counts from surf_count_location where surf = #{surf}")
+    Integer getSurfCountLocation(@Param("surf") Integer surf);
+
+    @Select("select counts from surf_count_information_class where surf = #{surf}")
+    Integer getSurfCountInformationClass(@Param("surf") Integer surf);
 }
