fdb7a4765dd050f89d7009c332513896853f1059
Tanxiao <13671985248@163.com>
2023-03-01 21:31:29 +0800

    LocationDto 及工厂的创建完毕

--- src/main/java/com/example/cse/Dto/LocationDto.java
+++ src/main/java/com/example/cse/Dto/LocationDto.java
@@ -1,8 +1,33 @@
 package com.example.cse.Dto;
 
+import com.example.cse.Entity.InformationClass.InformationClass;
 import com.example.cse.Entity.InformationClass.Location;
+import com.example.cse.Entity.InformationClass.Map;
+import com.example.cse.Entity.InformationClass.Message;
 
-public class LocationDto extends Location {
+import java.util.List;
+
+public class LocationDto  {
+
+    private Integer Lid;//唯一标识
+    private String Name;
+    private String Resume;
+    private String Ability;
+    private Message BasicMessage;
+
+
+
+    private Map MapBelong;
+    private Map MapOwn;
+    private String imgHref;//所携带的图片
+    private Integer X;//所在地图坐标
+    private Integer Y;
+
+    private List<Message> messages;
+    private List<InformationClass> informationClasses;
+    private List<MessageDto> messageShows;
+    private List<InformationClassDto> informationShows;
+
 
     public LocationDto() {
 
@@ -12,9 +37,119 @@ public class LocationDto extends Location {
         setLid(location.getLid());
         setAbility(location.getAbility());
         setImgHref(location.getImgHref());
-        setMapBelong(location.getMapBelong());
         setName(location.getName());
         setResume(location.getResume());
-        setMapOwn(location.getMapOwn());
+    }
+
+    public Integer getLid() {
+        return Lid;
+    }
+
+    public void setLid(Integer lid) {
+        Lid = lid;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getResume() {
+        return Resume;
+    }
+
+    public void setResume(String resume) {
+        Resume = resume;
+    }
+
+    public String getAbility() {
+        return Ability;
+    }
+
+    public void setAbility(String ability) {
+        Ability = ability;
+    }
+
+    public Message getBasicMessage() {
+        return BasicMessage;
+    }
+
+    public void setBasicMessage(Message basicMessage) {
+        BasicMessage = basicMessage;
+    }
+
+    public Map getMapBelong() {
+        return MapBelong;
+    }
+
+    public void setMapBelong(Map mapBelong) {
+        MapBelong = mapBelong;
+    }
+
+    public Map getMapOwn() {
+        return MapOwn;
+    }
+
+    public void setMapOwn(Map mapOwn) {
+        MapOwn = mapOwn;
+    }
+
+    public String getImgHref() {
+        return imgHref;
+    }
+
+    public void setImgHref(String imgHref) {
+        this.imgHref = imgHref;
+    }
+
+    public Integer getX() {
+        return X;
+    }
+
+    public void setX(Integer x) {
+        X = x;
+    }
+
+    public Integer getY() {
+        return Y;
+    }
+
+    public void setY(Integer y) {
+        Y = y;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    public List<InformationClass> getInformationClasses() {
+        return informationClasses;
+    }
+
+    public void setInformationClasses(List<InformationClass> informationClasses) {
+        this.informationClasses = informationClasses;
+    }
+
+    public List<MessageDto> getMessageShows() {
+        return messageShows;
+    }
+
+    public void setMessageShows(List<MessageDto> messageShows) {
+        this.messageShows = messageShows;
+    }
+
+    public List<InformationClassDto> getInformationShows() {
+        return informationShows;
+    }
+
+    public void setInformationShows(List<InformationClassDto> informationShows) {
+        this.informationShows = informationShows;
     }
 }
--- src/main/java/com/example/cse/Utils/Factory/LocationDtoFactory.java
+++ src/main/java/com/example/cse/Utils/Factory/LocationDtoFactory.java
@@ -1,4 +1,62 @@
 package com.example.cse.Utils.Factory;
 
+import com.example.cse.Dto.InformationClassDto;
+import com.example.cse.Dto.LocationDto;
+import com.example.cse.Dto.MessageDto;
+import com.example.cse.Dto.UserDto;
+import com.example.cse.Entity.InformationClass.InformationClass;
+import com.example.cse.Entity.InformationClass.Location;
+import com.example.cse.Entity.InformationClass.Message;
+import com.example.cse.Mapper.InformationClassMapper;
+import com.example.cse.Mapper.MapMapper;
+import com.example.cse.Mapper.MessageMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class LocationDtoFactory {
+    @Autowired
+    MessageMapper messageMapper;
+    @Autowired
+    MapMapper mapMapper;
+    @Autowired
+    InformationClassMapper informationClassMapper;
+    @Autowired
+    MessageDtoFactory messageDtoFactory;
+    @Autowired
+    InformationClassDtoFactory informationClassDtoFactory;
+
+    public LocationDto getLocationDto(Location location) {
+        LocationDto locationDto = new LocationDto(location);
+        locationDto.setBasicMessage(messageMapper.getMessageByRule(location.getBasicMessage(),null,null).get(0));
+        locationDto.setMapBelong(mapMapper.getMapByMid(location.getMapBelong()));
+        locationDto.setMapOwn(mapMapper.getMapByMid(location.getMapOwn()));
+        locationDto.setInformationClasses(informationClassMapper.getInformationClassByRule(null,null, locationDto.getLid()));
+        locationDto.setMessages(messageMapper.getMessageByRule(null,null, locationDto.getLid()));
+        return locationDto;
+    }
+
+    public void calculateShow(UserDto userDto,LocationDto locationDto, Integer informationLimit, Integer messageLimit, Integer informationMessageLimit) {
+        List<InformationClassDto> informationShows = informationClassDtoFactory.getInformationClassDtosByRankScore(locationDto.getInformationClasses(), userDto);
+
+        ArrayList<InformationClassDto> deletes = new ArrayList<>();
+        for (InformationClassDto informationClassDto:informationShows) {
+            if (informationClassDtoFactory.calculateShowMessages(informationClassDto,userDto,informationMessageLimit)) {
+                deletes.add(informationClassDto);
+            }
+        }
+        informationShows.removeAll(deletes);
+
+        locationDto.setInformationShows(informationShows);
+
+        List<MessageDto> messageShows = messageDtoFactory.getMessageDtosOrderByRankScore(locationDto.getMessages(), userDto);
+        if (messageShows.size()>messageLimit) {
+            locationDto.setMessageShows(messageShows.subList(0,messageLimit));
+        }else {
+            locationDto.setMessageShows(messageShows);
+        }
+    }
 }
