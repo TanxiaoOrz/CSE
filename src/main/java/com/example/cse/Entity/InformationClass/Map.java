fb5dcbcd586023557c24a89d1ba3df99f32094d9
Tanxiao <13671985248@163.com>
2023-03-02 22:04:07 +0800

    Map创建getter和setter

--- src/main/java/com/example/cse/Entity/InformationClass/Map.java
+++ src/main/java/com/example/cse/Entity/InformationClass/Map.java
@@ -2,11 +2,41 @@ package com.example.cse.Entity.InformationClass;
 
 public class Map {
     private Integer Mid;//
-
-    private Integer Location;//
     private String Href;//
     private String Name;//
     private String Resume;//
 
-    private Location location;
+
+    public Integer getMid() {
+        return Mid;
+    }
+
+    public void setMid(Integer mid) {
+        Mid = mid;
+    }
+
+
+    public String getHref() {
+        return Href;
+    }
+
+    public void setHref(String href) {
+        Href = href;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getResume() {
+        return Resume;
+    }
+
+    public void setResume(String resume) {
+        Resume = resume;
+    }
 }
