5d6bbe553b590e7a71fc815ff4edaac9300b94ab
Tanxiao <13671985248@163.com>
2023-03-04 22:07:41 +0800

    MessageMapper 新增对索引表的操作

--- src/main/java/com/example/cse/Mapper/MessageMapper.java
+++ src/main/java/com/example/cse/Mapper/MessageMapper.java
@@ -1,8 +1,7 @@
 package com.example.cse.Mapper;
 
 import com.example.cse.Entity.InformationClass.Message;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -18,5 +17,23 @@ public interface MessageMapper {
 
     String getMessageClassType(@Param("Mid") Integer Mid);
 
+    Integer newMessageRelationClass(@Param("Mid")Integer mid, @Param("Cid") Integer cid);
+
+    Integer newMessageRelationLocation(@Param("Mid")Integer mid, @Param("Lid") Integer lid);
+
+    @Delete("delete from message_information_class where Mid = #{Mid} and Cid = #{Cid}")
+    Integer deleteMessageRelationClass(@Param("Mid")Integer mid, @Param("Cid") Integer cid);
+
+    @Delete("delete from message_location where Mid = #{Mid} and Lid = #{Lid}")
+    Integer deleteMessageRelationLocation(@Param("Mid")Integer mid, @Param("Lid") Integer lid);
+
+    @Select("select Cid from message_information_class where Mid =#{Mid}")
+    List<Integer> getMessageRelationCid(@Param("Mid") Integer mid);
+
+    @Select("select Lid from message_location where Mid =#{Mid}")
+    List<Integer> getMessageRelationLid(@Param("Mid") Integer mid);
+
+    @Update("update message set DeprecatedFlag = 1 where Mid = #{Mid};")
+    Integer deleteMessage(@Param("Mid") Integer mid);
 
 }
