145aaa30682d93247921f30217f6f8af8b47692b
Tanxiao <13671985248@163.com>
2023-03-19 23:48:06 +0800

    LocationMapper 新建获取与Cid相关的Lid和Location

--- src/main/java/com/example/cse/Mapper/LocationMapper.java
+++ src/main/java/com/example/cse/Mapper/LocationMapper.java
@@ -3,6 +3,7 @@ package com.example.cse.Mapper;
 import com.example.cse.Entity.InformationClass.Location;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
@@ -22,4 +23,10 @@ public interface LocationMapper {
     List<Location> searchLocation(@Param("Defaults")List<String> defaults,
                                   @Param("Adds")List<String> adds,
                                   @Param("Minuses")List<String> minuses);
+
+    @Select("select * from location where Lid in (select Lid from information_class_location where Cid = #{Cid})")
+    List<Location> getLocationByCid(@Param("Cid") Integer cid);
+
+    @Select("select Lid from location where Lid in (select Lid from information_class_location where Cid = #{Cid})")
+    List<Integer> getLidByCid(@Param("Cid") Integer cid);
 }
