f88f20e30c628610c4ea6f3045ef3e1b55a97c3a
Tanxiao <13671985248@163.com>
2023-03-25 17:26:01 +0800

    FileController 描述更新

--- src/main/java/com/example/cse/Controller/FileController.java
+++ src/main/java/com/example/cse/Controller/FileController.java
@@ -7,10 +7,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -23,7 +20,7 @@ public class FileController {
     FileServiceImpl fileService;
 
     @PostMapping(value = "/Manager/upload")
-    @ApiOperation(value = "图片上传", notes = "图片上传")
+    @ApiOperation(value = "图片或图片上传", notes = "图片或图片上传")
     // 此处的@RequestParam中的file名应与前端upload组件中的name的值保持一致
     public Vo<String> upload(@RequestPart("file") MultipartFile multipartFile) throws WrongDataException, IOException {
         return new Vo<>(fileService.upload(multipartFile));
