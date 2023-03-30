5e7397579fd20309a82ba7117089c85e3d6f6610
Tanxiao <13671985248@163.com>
2023-03-31 01:18:28 +0800

    BasicModelDto 新增基本模型类 ModelMapper 增加基本模型的修改和查询

---
+++ src/main/java/com/example/cse/Dto/BasicModelDto.java
@@ -0,0 +1,13 @@
+package com.example.cse.Dto;
+
+public class BasicModelDto extends ModelDto {
+    Integer bid;
+
+    public Integer getBid() {
+        return bid;
+    }
+
+    public void setBid(Integer bid) {
+        this.bid = bid;
+    }
+}
--- src/main/java/com/example/cse/Mapper/ModelMapper.java
+++ src/main/java/com/example/cse/Mapper/ModelMapper.java
@@ -1,9 +1,11 @@
 package com.example.cse.Mapper;
 
+import com.example.cse.Dto.BasicModelDto;
 import com.example.cse.Dto.ModelDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -15,4 +17,13 @@ public interface ModelMapper {
 
     @Select("SELECT * from basic_model where Bid in (SELECT Bid from profession_basic_model where Pid = #{Pid})")
     List<ModelDto> getModelByProfession(@Param("Pid")Integer pid);
+
+    @Select("SELECT * from basic_model where Bid in (SELECT Bid from profession_basic_model where Pid = #{Pid})")
+    List<BasicModelDto> getBasicModelByProfession(@Param("Pid")Integer pid);
+
+    @Select("SELECT * from basic_model where Bid in (SELECT Bid from year_basic_model where Year = #{Year})")
+    List<BasicModelDto> getBasicModelByYear(@Param("Year")Integer year);
+
+    @Update("update basic_model set score = #{score} where Bid = #{bid}")
+    Integer updateBasicModels(BasicModelDto basicModelDto);
 }
