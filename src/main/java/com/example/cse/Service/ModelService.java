e46b4c7f2ab60c1219c13fe4ffca6b51a4cdbce0
Tanxiao <13671985248@163.com>
2023-03-31 01:22:33 +0800

    ModelService 定义模型更新的行为 calculateHobbyModel regenerateHobbyModel calculateBasicModelScore

---
+++ src/main/java/com/example/cse/Service/ModelService.java
@@ -0,0 +1,11 @@
+package com.example.cse.Service;
+
+public interface ModelService {
+
+    void calculateHobbyModel();
+
+    void regenerateHobbyModel();
+
+    void calculateBasicModelScore();
+
+}
