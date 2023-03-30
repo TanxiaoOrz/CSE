755c896fa49633d02dd3582167cf78674c36d9b1
Tanxiao <13671985248@163.com>
2023-03-31 01:19:44 +0800

    HobbyMapper 新增爱好模型的修改方法

--- src/main/java/com/example/cse/Mapper/HobbyMapper.java
+++ src/main/java/com/example/cse/Mapper/HobbyMapper.java
@@ -3,6 +3,7 @@ package com.example.cse.Mapper;
 import com.example.cse.Entity.Recommend.Hobby;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -10,6 +11,9 @@ import java.util.List;
 public interface HobbyMapper {
 
     Integer newHobby(Hobby hobby);
+
+    @Update("UPDATE hobby set Model = #{Model} where Hid = #{Hid}")
+    Integer updateHobbyModel(@Param("Hid")Integer hid,@Param("Model")String model);
 
     Hobby getHobbyByRule(Hobby hobby);
 
