0ddf9f4d6080ede336d2262ea4fee123cbafa463
Tanxiao <13671985248@163.com>
2023-03-31 22:06:43 +0800

    ManagerService 新增管理员设置获取与修改

--- src/main/java/com/example/cse/Service/ManagerService.java
+++ src/main/java/com/example/cse/Service/ManagerService.java
@@ -1,9 +1,15 @@
 package com.example.cse.Service;
 
+import com.example.cse.Utils.Exception.WrongDataException;
+import com.example.cse.Vo.ManagerConfig;
 import com.example.cse.Vo.UserPass;
 
 import java.util.List;
 
 public interface ManagerService {
     boolean checkManager(UserPass userPass);
+
+    ManagerConfig getConfig();
+
+    boolean updateConfig(ManagerConfig managerConfig) throws WrongDataException;
 }
--- src/main/java/com/example/cse/Service/impl/ManagerServiceImpl.java
+++ src/main/java/com/example/cse/Service/impl/ManagerServiceImpl.java
@@ -1,7 +1,13 @@
 package com.example.cse.Service.impl;
 
 import com.example.cse.Service.ManagerService;
+import com.example.cse.Task.CalculateTask;
+import com.example.cse.Task.DeleteTask;
+import com.example.cse.Task.EndOpenTask;
+import com.example.cse.Utils.Exception.WrongDataException;
+import com.example.cse.Vo.ManagerConfig;
 import com.example.cse.Vo.UserPass;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +19,54 @@ public class ManagerServiceImpl implements ManagerService {
 
     @Value("${manager.managerPass}")
     String managerPass;
+    @Autowired
+    CalculateTask calculateTask;
+    @Autowired
+    DeleteTask deleteTask;
+    @Autowired
+    EndOpenTask endOpenTask;
 
 
     @Override
     public boolean checkManager(UserPass userPass) {
         return userPass.getUserCode().equals(managerName)&&userPass.getUserPass().equals(managerPass);
     }
+
+    @Override
+    public ManagerConfig getConfig() {
+        ManagerConfig managerConfig = new ManagerConfig();
+        managerConfig.setCalculateDegree(calculateTask.getCalculateDegree());
+        managerConfig.setCalculateEnabled(calculateTask.isCalculateEnabled());
+        managerConfig.setSleepEnabled(endOpenTask.isSleepEnabled());
+        managerConfig.setDeleteEnabled(deleteTask.isDeleteEnabled());
+        return managerConfig;
+    }
+
+    @Override
+    public boolean updateConfig(ManagerConfig managerConfig) throws WrongDataException {
+        boolean updated = false;
+        managerConfig.checkValue();
+        if (managerConfig.getSleepEnabled() != endOpenTask.isSleepEnabled()) {
+            endOpenTask.setSleepEnabled(managerConfig.getSleepEnabled());
+            updated = true;
+        }
+        if (managerConfig.getSleepEnabled()) {
+            if (managerConfig.getDeleteEnabled() != deleteTask.isDeleteEnabled()) {
+                deleteTask.setDeleteEnabled(managerConfig.getDeleteEnabled());
+                updated = true;
+            }
+            if (managerConfig.getCalculateEnabled() != (calculateTask.isCalculateEnabled())) {
+                calculateTask.setCalculateEnabled(managerConfig.getCalculateEnabled());
+                updated = true;
+            }
+            if (managerConfig.getCalculateDegree() != calculateTask.getCalculateDegree()) {
+                calculateTask.setCalculateDegree(managerConfig.getCalculateDegree());
+                updated = true;
+            }
+        }
+
+        return updated;
+    }
+
 
 }
