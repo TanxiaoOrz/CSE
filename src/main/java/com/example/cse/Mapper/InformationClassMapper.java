d04f8a1d505a39e8792ee1e5d378ab9bcaa488aa
Tanxiao <13671985248@163.com>
2023-05-23 13:44:42 +0800

    InformationClassMapper sql语句错误

--- src/main/java/com/example/cse/Mapper/InformationClassMapper.java
+++ src/main/java/com/example/cse/Mapper/InformationClassMapper.java
@@ -8,31 +8,31 @@ import java.util.List;
 @Mapper
 public interface InformationClassMapper {
 
-    List<InformationClass> getInformationClassByRule(@Param("Cid") Integer cid,
-            @Param("RelativeMessage") Integer relativeMessage, @Param("RelativeLocation") Integer lid);
+        List<InformationClass> getInformationClassByRule(@Param("Cid") Integer cid,
+                        @Param("RelativeMessage") Integer relativeMessage, @Param("RelativeLocation") Integer lid);
 
-    Integer newInformationClass(InformationClass informationClass);
+        Integer newInformationClass(InformationClass informationClass);
 
-    Integer updateInformationClass(InformationClass informationClass);
+        Integer updateInformationClass(InformationClass informationClass);
 
-    @Update("update information_class set DeprecatedFlag = 1 where Cid = #{Cid}")
-    Integer deleteInformationClass(@Param("Cid") Integer cid);
+        @Update("update information_class set DeprecatedFlag = 1 where Cid = #{Cid}")
+        Integer deleteInformationClass(@Param("Cid") Integer cid);
 
-    List<InformationClass> searchInformationClass(@Param("Type") String type,
-            @Param("Defaults") List<String> defaults,
-            @Param("Adds") List<String> adds,
-            @Param("Minuses") List<String> minuses);
+        List<InformationClass> searchInformationClass(@Param("Type") String type,
+                        @Param("Defaults") List<String> defaults,
+                        @Param("Adds") List<String> adds,
+                        @Param("Minuses") List<String> minuses);
 
-    @Insert("insert into information_class_location (Cid, Lid) VALUES (#{Cid}, #{Lid})")
-    Integer newInformationClassRelationLocation(@Param("Lid") Integer lid, @Param("Cid") Integer cid);
+        @Insert("insert into information_class_location (Cid, Lid) VALUES (#{Cid}, #{Lid})")
+        Integer newInformationClassRelationLocation(@Param("Lid") Integer lid, @Param("Cid") Integer cid);
 
-    @Delete("delete from information_class_location where Cid = #{Cid} and Lid = #{Lid}")
-    Integer deleteInformationClassRelationLocation(@Param("Lid") Integer lid, @Param("Cid") Integer cid);
+        @Delete("delete from information_class_location where Cid = #{Cid} and Lid = #{Lid}")
+        Integer deleteInformationClassRelationLocation(@Param("Lid") Integer lid, @Param("Cid") Integer cid);
 
-    @Select("select Lists ferom information_class_echarts where Cid = #{Cid}")
-    String getEchartData(@Param("Cid") Integer cid);
+        @Select("select Lists from information_class_echarts where Cid = #{Cid}")
+        String getEchartData(@Param("Cid") Integer cid);
 
-    @Select("select StartYear ferom information_class_echarts where Cid = #{Cid}")
-    Integer getEchartStartYear(@Param("Cid") Integer cid);
+        @Select("select StartYear from information_class_echarts where Cid = #{Cid}")
+        Integer getEchartStartYear(@Param("Cid") Integer cid);
 
 }
